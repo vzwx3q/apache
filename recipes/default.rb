88a4ed4fdcee3d01b9a7eb175b03e26b8d82392b
vzwx3q <gouniravi@gmail.com>
2016-10-18 15:01:53 +0000

    added recipe code

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,12 +4,26 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 #
-package "httpd" do
+if node["platform"] == "ubuntu"
+execute "apt-get update" do
+    command "apt-get update"
+end
+end
+
+package_name = "httpd"
+
+if node["platform"] == "ubuntu"
+package_name = "apache2"
+end
+
+package package_name do
    action :install
 end
-service "httpd" do
+
+service package_name do
   action [:enable, :start]
 end
+
 template "/var/www/html/index.html" do
   source "index.html.erb"
   mode "0644"
