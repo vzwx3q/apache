c524ee6254cfaac858aa8a3ca2434a58529b8497
vzwx3q <gouniravi@gmail.com>
2016-10-18 12:29:29 +0000

    aa

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+#
+package "httpd" do
+   action :install
+end
+service "httpd" do
+  action [:enable, :start]
+end
+template "/var/www/html/index.html" do
+  source "index.html.erb"
+  mode "0644"
+end
